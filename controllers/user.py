d62dafd81760162fe62a36deba105b91aeec59bf
Kobayashi-Yuto <kobayashi.works@fujitsu.com>
2021-07-16 09:57:50 +0900

    [refactor] ユーザ関連処理をモデルへ移動 / index処理をリスト内包表記に変更 #3

--- controllers/user.py
+++ controllers/user.py
@@ -1,9 +1,9 @@
 from typing_extensions import Final
-from flask import Blueprint, json, request, jsonify
-from flask_sqlalchemy_session import current_session
-from typing import Any, Dict, List
+from flask import Blueprint, request, jsonify
+from typing import Any, Dict
 from const import input_limit
 from models.user import User
+from const.message import NOT_FOUND, DUPLICATE_RECORD
 
 app = Blueprint('user', __name__, url_prefix='/user')
 USERNAME: Final[str] = "name"
@@ -30,52 +30,28 @@ def get_validate_errors(inputed_info: Dict[str, Any]) -> Dict[str, Any]:
 
   return errors_params
 
-def check_duplicate_username(username: str) -> bool:
-  """ユーザ名が重複しているか確認する
-
-  Args:
-      username (str): ユーザ名
-
-  Returns:
-      bool: ユーザ名が重複しているか
-  """
-  same_name_user: List[User] = current_session.query(User).filter(User.name == username).all()
-
-  return len(same_name_user) > 0
-
 @app.route('/', methods=['POST'])
 def create() -> Dict[str, Any]:
-  inputed_info = {
-    USERNAME: request.json.get('name', None),
-    NICKNAME: request.json.get('nickname', None),
-    PASSWORD: request.json.get('password', None)
-  }
-  validate_errors: Dict[str, Any] = get_validate_errors(inputed_info)
-  is_duplicate: bool = check_duplicate_username(inputed_info[USERNAME])
-  if (len(validate_errors) > 0):
-    return validate_errors
-  if is_duplicate:
-    return jsonify({"status": "ユーザ名が重複しています"})
-  user = User(name=inputed_info[USERNAME], password=inputed_info[PASSWORD], nickname=inputed_info[NICKNAME])
-  current_session.add(user)
-  current_session.commit()
+  body = request.json.get('user')
+  created_user_name: str = User.create(body)
+  if created_user_name == None:
+    return jsonify(DUPLICATE_RECORD['message']), DUPLICATE_RECORD['status']
 
-  return jsonify({ "status": "success" })
+  return jsonify({ "username": created_user_name })
 
-@app.route('/<user_id>', methods=['POST'])
+@app.route('/<user_id>', methods=['PUT'])
 def update(user_id: str) -> Dict[str, Any]:
-  validate_errors = get_validate_errors(request.json)
-  if len(validate_errors) > 0:
-    return validate_errors
-  
-  user: User = current_session.query(User).filter(User.id == user_id).first()
-  user.nickname = request.json.get('nickname', None)
-  current_session.commit()
+  body = request.json.get('user')
+  user: User = User.update(user_id, body)
+  if user == None:
+    return jsonify(NOT_FOUND['message']), NOT_FOUND['status']
 
   return jsonify(user.to_dict())
 
 @app.route('/<user_id>', methods=['GET'])
 def find(user_id: str):
-  user: User = current_session.query(User).filter(User.id == user_id).first()
+  user: User = User.find_by_id(user_id)
+  if user == None:
+    return jsonify(NOT_FOUND['message']), NOT_FOUND['status']
 
   return jsonify(user.to_dict())
