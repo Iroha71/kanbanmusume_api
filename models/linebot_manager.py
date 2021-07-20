a155de4636e4dcd2163a5b1711e4587ad02b0526
Kobayashi-Yuto <kobayashi.works@fujitsu.com>
2021-07-20 09:27:10 +0900

    [fix] .envが読み込めるようにパス結合方法を修正 #8

--- models/linebot_manager.py
+++ models/linebot_manager.py
@@ -1,15 +1,18 @@
-import os
+from os import path, environ
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 from dotenv import load_dotenv
+from pathlib import Path
 
-dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+dotenv_path = Path(__file__).parent
+dotenv_path /= '../'
+dotenv_path = path.join(dotenv_path.resolve(), '.env')
 load_dotenv(dotenv_path)
-
-LINE_ID: str = os.environ.get("LINE_ID")
-CHANNEL_TOKEN: str = os.environ.get("LINE_CHANNEL_TOKEN")
+LINE_ID: str = environ.get("LINE_ID")
+CHANNEL_TOKEN: str = environ.get("LINE_CHANNEL_TOKEN")
 linebot_api = LineBotApi(CHANNEL_TOKEN)
+
 def push_msg(taskname: str=None):
   try:
     linebot_api.push_message(LINE_ID, TextSendMessage(text="テスト"))
